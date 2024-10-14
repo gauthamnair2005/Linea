2875462f44f609c40bd03ffc8cd7c17bfde2c96b
Gautham Nair <gautham.nair.2005@outlook.com>
2024-10-14 19:13:58 +0530

    Website

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', function() {
+    const footer = document.getElementById('footer');
+    if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+        footer.style.display = 'block';
+    } else {
+        footer.style.display = 'none';
+    }
+});
